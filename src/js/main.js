3b400d79c944bc737f759f298c552897c6891456
dawidek14472 <dawid.okojew@o2.pl>
2023-10-22 20:06:55 +0200

    Added functionality when you click on menu then menu will be closed

--- src/js/main.js
+++ src/js/main.js
@@ -1,15 +1,20 @@
-const burgerBtn = document.querySelector('.burgerBtn');
-const menu = document.querySelector('.menu');
-const menuLinks = document.querySelectorAll('.menu-link'); // Zmieniłem `querySelector` na `querySelectorAll`
+const burgerBtn = document.querySelector('.burgerBtn')
+const menu = document.querySelector('.menu')
+const menuLinks = document.querySelectorAll('.menu-link')
 
 const btn = () => {
-    menu.classList.toggle('active');
+	menu.classList.toggle('active')
 
-    menuLinks.forEach(item => {
-        item.addEventListener('click', () => {
-            menu.classList.remove('active');
-        });
-    });
-};
+	menuLinks.forEach(item => {
+		item.addEventListener('click', () => {
+			menu.classList.remove('active')
+		})
+	})
+}
+const closeMenu =() =>{
+    menu.classList.remove('active')
+}
 
-burgerBtn.addEventListener("click", btn);
+
+burgerBtn.addEventListener('click', btn)
+menu.addEventListener('click',closeMenu)
