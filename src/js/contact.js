1da5bb64f529cedce5c05d8b44e3930601a719d2
dawidek14472 <dawid.okojew@o2.pl>
2023-10-26 19:21:23 +0200

    Added year to footer

--- src/js/contact.js
+++ src/js/contact.js
@@ -1,6 +1,12 @@
 const burgerBtn = document.querySelector('.burgerBtn')
 const menu = document.querySelector('.menu')
 const menuLinks = document.querySelectorAll('.menu-link')
+const footerYear = document.querySelector(".footer__year");
+const closeButton = document.querySelector('.menu__closeButton')
+const contactForm = document.getElementById('contact-form');
+const nameInput = document.getElementById('name');
+const emailInput = document.getElementById('email');
+const messageInput = document.getElementById('message');
 
 const btn = () => {
 	menu.classList.toggle('active')
@@ -16,25 +22,28 @@ const closeMenu =() =>{
 }
 
 
-burgerBtn.addEventListener('click', btn)
-menu.addEventListener('click',closeMenu)
-const contactForm = document.getElementById('contact-form');
-const nameInput = document.getElementById('name');
-const emailInput = document.getElementById('email');
-const messageInput = document.getElementById('message');
+const handleCurrentYear = () => {
+    const year = new Date().getFullYear();
+    footerYear.innerText = year;
+};
 
 contactForm.addEventListener('submit', (e) => {
     e.preventDefault();
-
-
+    
+    
     if (nameInput.value.trim() === '' || emailInput.value.trim() === '' || messageInput.value.trim() === '') {
         alert('Wypełnij wszystkie pola formularza.');
         return;
     }
-
+    
     nameInput.value = '';
     emailInput.value = '';
     messageInput.value = '';
-
+    
     alert('Formularz został wysłany!');
 });
+
+handleCurrentYear();
+burgerBtn.addEventListener('click', btn)
+menu.addEventListener('click',closeMenu)
+closeButton.addEventListener('click',closeMenu)
--- src/js/main.js
+++ src/js/main.js
@@ -1,6 +1,8 @@
 const burgerBtn = document.querySelector('.burgerBtn')
 const menu = document.querySelector('.menu')
 const menuLinks = document.querySelectorAll('.menu-link')
+const footerYear = document.querySelector(".footer__year");
+const closeButton = document.querySelector('.menu__closeButton')
 
 const btn = () => {
 	menu.classList.toggle('active')
@@ -14,7 +16,14 @@ const btn = () => {
 const closeMenu =() =>{
     menu.classList.remove('active')
 }
+const handleCurrentYear = () => {
+	const year = new Date().getFullYear();
+	footerYear.innerText = year;
+};
+
+handleCurrentYear();
 
 
 burgerBtn.addEventListener('click', btn)
 menu.addEventListener('click',closeMenu)
+closeButton.addEventListener('click',closeMenu)
